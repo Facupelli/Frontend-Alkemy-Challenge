8cc0d072de427db5944b2aa43bc0b16bd3824e64
Facupelli <facundopellicer4@gmail.com>
2022-02-03 17:28:43 -0300

    can not add more than 4 plates

---
+++ client/src/components/MenuWarning.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Modal from "react-bootstrap/Modal";
+
+export const MenuWarning = ({ modal, setModal }) => {
+  const handleClose = () => {
+    setModal(false);
+  };
+
+  return (
+    <Modal
+      show={modal}
+      onHide={handleClose}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title className="text-danger ">Warning</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="form-floating mb-3 fs-5">
+          <p className="m-0">You already have 4 plates on the menu!</p>
+          <p>please remove one to add another.</p>
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
+};
--- client/src/components/Plate.jsx
+++ client/src/components/Plate.jsx
@@ -5,42 +5,59 @@ import { Link } from "react-router-dom";
 import { StoreContext } from "../context/context";
 import { useContext } from "react";
 import { getRecipeById } from "../info-api/api";
-
+import { useState } from "react";
+import { MenuWarning } from "./MenuWarning";
 
 export const Plate = ({ image, title, search, id, menu }) => {
-
   const { menuPlates, setMenuPlates } = useContext(StoreContext);
 
-  const handleRemove = () => {
-    console.log('ENTRE')
-    const plates = menuPlates.filter(el => id !== el.id )
-    console.log('REMOVE', plates)
-    setMenuPlates(plates)
-  }
+  const [modal, setModal] = useState(false);
 
-  const handleAddToMenu = async() => {
+  const handleRemove = () => {
+    console.log("ENTRE");
+    const plates = menuPlates.filter((el) => id !== el.id);
+    console.log("REMOVE", plates);
+    setMenuPlates(plates);
+  };
+
+  const handleAddToMenu = async () => {
     const plate = await getRecipeById(id);
-    setMenuPlates([...menuPlates, plate])
-  }
+    if (menuPlates.length < 4) {
+      setMenuPlates([...menuPlates, plate]);
+    } else {
+      setModal(true);
+    }
+  };
 
   return (
-    <Card style={{ width: menu ? "25rem" : "15rem" }}>
-      <Link to={`/recipe/${id}`}>
-        <Card.Img
-          variant="top"
-          src={image}
-          className="img-fluid"
-          alt="Card image"
-        />
-      </Link>
-      <Card.Body>
-        <Card.Title className="text-dark">{title}</Card.Title>
-        {/* <Button variant="primary" className="">
+    <>
+      {modal && <MenuWarning modal={modal} setModal={setModal} />}
+      <Card style={{ width: menu ? "25rem" : "15rem" }}>
+        <Link to={`/recipe/${id}`}>
+          <Card.Img
+            variant="top"
+            src={image}
+            className="img-fluid"
+            alt="Card image"
+          />
+        </Link>
+        <Card.Body>
+          <Card.Title className="text-dark">{title}</Card.Title>
+          {/* <Button variant="primary" className="">
           See more
         </Button> */}
-        {search && <Button variant="primary" onClick={handleAddToMenu}>Add to Menu</Button>}
-        {menu && <Button variant="danger" onClick={handleRemove}>Remove</Button>}
-      </Card.Body>
-    </Card>
+          {search && (
+            <Button variant="primary" onClick={handleAddToMenu}>
+              Add to Menu
+            </Button>
+          )}
+          {menu && (
+            <Button variant="danger" onClick={handleRemove}>
+              Remove
+            </Button>
+          )}
+        </Card.Body>
+      </Card>
+    </>
   );
 };
