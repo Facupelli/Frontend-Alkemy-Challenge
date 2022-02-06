56ffe44cca08b0de99838017d7ae64243c9c24b0
Facupelli <facundopellicer4@gmail.com>
2022-02-05 23:31:39 -0300

    deploy settings

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import StoreProvider from './context/context'
 
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
